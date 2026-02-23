443eaefcb3cc601cc804b79c15b8aa2e9400fd20
Dipanshu <dipanshu639722@gmail.com>
2026-02-23 23:07:59 +0530

    Adding data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project uses machine learning algorithms to analyze insurance claim data and identify suspicious or fraudulent claims. By learning patterns from historical data, the model helps insurers reduce losses, improve claim accuracy, and speed up decision-making.',
+    author='The Optimizers',
+    license='',
+)
