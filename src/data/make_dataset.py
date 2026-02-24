a496417b85fe867b4c3cba7a91c34239eb19d0cb
Dipanshu <dipanshu639722@gmail.com>
2026-02-24 11:41:05 +0530

    Edit in  make_dataset in src

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,53 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import numpy as np
+import os
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def make_dataset(input_path, output_path):
+
+    print("Reading raw dataset...")
+    df = pd.read_csv(input_path)
+
+    df.replace('?', np.nan, inplace=True)
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    df["collision_type"].fillna(  df["collision_type"].mode()[0], inplace=True)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+    df["property_damage"].fillna(df["property_damage"].mode()[0],inplace=True)
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    df["police_report_available"].fillna(df["police_report_available"].mode()[0],inplace=True)
 
-    main()
+    print("Missing values handled")
+
+    to_drop = [
+        'policy_number',
+        'policy_bind_date',
+        'policy_state',
+        'insured_zip',
+        'incident_location',
+        'incident_date',
+        'incident_state',
+        'incident_city',
+        'insured_hobbies',
+        'auto_make',
+        'auto_model',
+        'auto_year',
+        '_c39'
+    ]
+
+    df.drop(columns=to_drop, inplace=True, errors="ignore")
+
+ 
+    df.drop(columns=['age', 'total_claim_amount'],inplace=True,errors="ignore")
+
+    print("Unnecessary columns dropped ")
+
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    df.to_csv(output_path, index=False)
+
+    print(f"Cleaned dataset saved at: {output_path} ")
+
+if __name__ == "__main__":
+
+    make_dataset(input_path="data/raw/insurance_claims.csv",output_path="data/processed/cleaned_insurance_claims.csv")
